ad3b21ccbc4f552d1970779d14057264d9d8aa3b
emdad <emdad.hossain321@gmail.com>
2019-06-13 00:51:19 +0600

    subcomponent added

--- app/src/main/java/com/saltandpepper/emdad/daggerwithkotlin/ScopeActivity.kt
+++ app/src/main/java/com/saltandpepper/emdad/daggerwithkotlin/ScopeActivity.kt
@@ -3,23 +3,30 @@ package com.saltandpepper.emdad.daggerwithkotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import dagger.Component
+import dagger.Subcomponent
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_scope.*
 import javax.inject.Inject
 import javax.inject.Scope
+import javax.inject.Singleton
 
- var staticCounter = 0;
+var staticCounter = 0;
 
-private lateinit var magicBox: DaggerScopeActivity_MagicBox
+
+private lateinit var mainBox:DaggerScopeActivity_SingleToneBox
+private lateinit var magicBox: ScopeActivity.MagicBox
+
 class ScopeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scope)
-        magicBox = DaggerScopeActivity_MagicBox.create() as DaggerScopeActivity_MagicBox
+        mainBox = DaggerScopeActivity_SingleToneBox.create() as DaggerScopeActivity_SingleToneBox
+        magicBox = mainBox.getMagicBox()
 
         button.setOnClickListener {
             val storage = Storage()
-            magicBox.poke(storage)
+            magicBox = mainBox.getMagicBox()
+            useStorage()
             textView.text = "Unique ${storage.uniqueMagic.count}" + " " +
                     "\nNormal  ${storage.normalMagic.count}"
         }
@@ -27,6 +34,11 @@ class ScopeActivity : AppCompatActivity() {
 
 
 
+    }
+
+    @Singleton
+    class SingleTone @Inject constructor(){
+        val count = staticCounter++
     }
     @MagicScope
     class UniqueMagic @Inject constructor(){
@@ -44,15 +56,30 @@ class ScopeActivity : AppCompatActivity() {
 
 
     class Storage{
+        @Inject lateinit var singletonOne:SingleTone
         @Inject lateinit var uniqueMagic: UniqueMagic
         @Inject lateinit var normalMagic: NormalMagic
     }
 
-    @MagicScope
+
+    @Singleton
     @Component
+    interface SingleToneBox{
+        fun getMagicBox():MagicBox
+    }
+
+    @MagicScope
+    @Subcomponent
     interface MagicBox{
         fun poke(storage:Storage)
     }
 
-
+    private fun useStorage() {
+        val storage = Storage()
+        magicBox.poke(storage)
+        textView.text =
+                "\nSingletonOne ${storage.singletonOne.count} " +
+                "\nUniqueMagic ${storage.uniqueMagic.count}" +
+                "\nNormalMagic ${storage.normalMagic.count} "
+    }
 }
